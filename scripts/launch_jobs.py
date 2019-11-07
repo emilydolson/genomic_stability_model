04bb4bc97571a3667a87332c5f38c198570c4442
Emily Dolson <EmilyLDolson@gmail.com>
2019-11-07 11:22:48 -0500

    wrote job launch script

---
+++ scripts/launch_jobs.py
@@ -0,0 +1,29 @@
+import sys
+
+ks = eval(sys.argv[1])
+ss = eval(sys.argv[2])
+ms = eval(sys.argv[3])
+
+for k in ks:
+    for s in ss:
+        for m in ms:
+            with open(f"k_{k}_s{s}_m{m}.sbatch", "w") as outfile:
+                outfile.write(f"""#!/bin/bash --login                                               
+########## SBATCH Lines for Resource Request ##########                         
+
+#SBATCH --time=3:59:00
+#SBATCH --job-name m{m}_k{k}_mult{s}                                               
+####SBATCH --array=1-10                                                            
+########## Command Lines to Run ##########                                      
+
+cd /mnt/scratch/dolsonem/genomic_stability/data/MUTPROB_{m}_K_{k}_FITNESSMULT_{s}
+
+#mkdir $SLURM_ARRAY_TASK_ID
+#cd $SLURM_ARRAY_TASK_ID
+for i in {{1..10}}
+do
+    mkdir $i
+    cd $i
+    ../../../genomic_stability_model -TIME_STEPS 1000000 -INITIAL_FITNESS 1 -GAMMA_K {k} -MUT_PROB {m} -FITNESS_MULT {s} -INIT_POP_SIZE 500 > run.log
+done
+""")
